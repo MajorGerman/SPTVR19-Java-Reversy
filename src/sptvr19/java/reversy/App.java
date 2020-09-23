1acbfdd82048c1583cfc4fe6dce2284b8547bf67
Killerson <georg.laabe@ivkhk.ee>
2020-09-23 10:34:32 +0300

    Готово

--- src/sptvr19/java/reversy/App.java
+++ src/sptvr19/java/reversy/App.java
@@ -1,8 +1,35 @@
 package sptvr19.java.reversy;
 
+import java.util.Scanner;
+
 class App {
     
-    public void run() {
-        //
+    public void run() throws InterruptedException {
+        
+        System.out.println(" -- Reversy Game -- ");
+        System.out.println(" -- by Georg Laabe -- ");
+        
+        System.out.println("\n This programm will return inputed string reversed");
+        
+        Scanner scan = new Scanner(System.in);
+              
+        int i;
+        
+        System.out.print("\nInput your string: ");
+        String string = scan.nextLine();
+        
+        char[] array = new char[string.length()];
+        char[] chara = string.toCharArray();
+                
+        for (i = 0; i < string.length(); i++) {
+            array[i] = chara[string.length()-i-1];
+        }
+        
+        string = new String(array);
+        string = string.toLowerCase();
+        string = string.substring(0, 1).toUpperCase() + string.substring(1);
+        System.out.println("Reversed string: " + string);     
+        
+        Thread.sleep(4500);
     }
 }
